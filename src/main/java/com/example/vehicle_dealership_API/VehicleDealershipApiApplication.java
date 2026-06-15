2cdcbaf637529669bdbfabf312e1c6b26851c5aa
yathisha-coder <yathisai85@gmail.com>
2026-06-15 14:42:12 -0400

    Initial commit

---
+++ src/main/java/com/example/vehicle_dealership_API/VehicleDealershipApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_dealership_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDealershipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDealershipApiApplication.class, args);
+	}
+
+}
